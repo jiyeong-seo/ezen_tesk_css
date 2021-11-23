bf49c0ea8db371bc2e373017eaea960a910593b4
sjy <jen.jyseo@gmail.com>
2021-11-23 22:59:32 +0900

    commit

---
+++ ezen_test/1119_three_test/starbucks.js
@@ -0,0 +1,16 @@
+/**
+ * @filename : starbucks.js
+ * @author : 서지영(jen.jyseo@gmail.com)
+ * @description : 스타벅스 메인 페이지를 제어하기 위한 CSS코드
+ */
+
+ class Starbucks {
+/* 상단 스타벅스 회원 메뉴 버튼 클릭시 호출
+======================================== */
+  /**
+   * @param {string} selector 입력요소에 해당하는 CSS 선택자
+   * @param {string} msg 값이 없을 경우 표시할 메시지 내용
+   * @return {boolean} 입력된 경우 true / 입력되지 않은 경우 false
+   */
+  
+ };
